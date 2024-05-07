848f136cf88589a275ff75535486f7cf54a73b90
cmy0357 <mic015@ucsd.edu>
2024-05-07 05:41:57 -0700

    Finish explore part 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Lab 5 - Mingyang Chen
+https://cmy0357.github.io/sp24-cse110-lab5/expose.html
+https://cmy0357.github.io/sp24-cse110-lab5/explore.html
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,46 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  let select = document.getElementById('voice-select');
+  let btn = document.querySelector('button');
+  let inputText = document.querySelector('textarea');
+  let image = document.querySelector('img');
+
+  const synth = window.speechSynthesis;
+  let voices = [];
+  function populateVoiceList() {
+    voices = synth.getVoices();
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      select.appendChild(option);
+    }
+  }
+
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  btn.addEventListener('click', function() {
+    const utterThis = new SpeechSynthesisUtterance(inputText.value);
+    const selected = select.selectedOptions[0].getAttribute('data-name');
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selected) {
+        utterThis.voice = voices[i];
+        break;
+      }
+    }
+    synth.speak(utterThis);
+
+    utterThis.addEventListener('start', function() {
+      image.src = 'assets/images/smiling-open.png';
+    })
+
+    utterThis.addEventListener('end', function() {
+      image.src = 'assets/images/smiling.png';
+    })
+  })
 }
